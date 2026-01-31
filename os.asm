53d35e96e7d0f9b1c5f883f3ac13cc6083677c80
PAZ-tech-real <patrickmcsorley16@gmail.com>
2026-01-31 18:28:46 +0000

    Update boot_sound routine

--- os.asm
+++ os.asm
@@ -1,159 +1,236 @@
+bits 16
 org 0x7C00
 
-; --- Set text mode (80x25) ---
-mov ax, 0x03
-int 0x10
-mov si, copy_msg
-call print_string
+start:
+; read sector 2 from first HDD (0x80) into buffer
+    mov ah,0x02        ; BIOS: read sectors
+    mov al,1           ; number of sectors
+    mov ch,0           ; cylinder
+    mov cl,2           ; sector number
+    mov dh,0           ; head
+    mov dl,0x80        ; drive = first HDD
+    mov bx,slot        ; buffer to store data
+    int 13h
+    mov ax,0x03
+    int 0x10
+    call boot_sound
+    mov si,msg_start
+    call print
 
-; --- Shell loop ---
-shell_loop:
-    call newline
-    mov si, prompt
-    call print_string
-
-    mov di, buffer
+shell:
+    call nl
+    mov si,prompt
+    call print
+    mov di,buf
     call read_line
 
-    ; Check for "input"
-    mov si, buffer
-    mov di, input_cmd
-    call str_compare
-    cmp al, 1
+    mov si,buf
+    mov di,cmd_in
+    call cmp
+    cmp al,1
     je do_input
 
-    ; Check for "echo"
-    mov si, buffer
-    mov di, echo_cmd
-    call str_compare
-    cmp al, 1
+    mov si,buf
+    mov di,cmd_echo
+    call cmp
+    cmp al,1
     je do_echo
 
-    ; Check for "read"
-    mov si, buffer
-    mov di, read_cmd
-    call str_compare
-    cmp al, 1
+    mov si,buf
+    mov di,cmd_read
+    call cmp
+    cmp al,1
     je do_read
 
-    mov si, unknown_msg
-    call newline
-    call print_string
-    jmp shell_loop
+    mov si,buf
+    mov di,cmd_reboot
+    call cmp
+    cmp al,1
+    je do_reboot
 
-; -------------------------------
+    mov si,buf
+    mov di,cmd_shut
+    call cmp
+    cmp al,1
+    je do_shutdown
+
+    mov si,msg_unknown
+    call nl
+    call print
+    jmp shell
+
+; --- commands ---
 do_input:
-    mov si, input_msg
-    call newline
-    call print_string
-    mov di, slot
+    call nl
+    mov di,slot
     call read_line
-    mov si, saved_msg
-    call newline
-    call print_string
-    jmp shell_loop
+    jmp shell
 
 do_echo:
-    mov si, buffer+5
-    call newline
-    call print_string
-    jmp shell_loop
+    mov si,buf+5
+    call nl
+    call print
+    jmp shell
 
 do_read:
-    mov si, slot
-    call newline
-    call print_string
-    jmp shell_loop
+    mov si,slot
+    call nl
+    call print
+    jmp shell
 
-; -------------------------------
-print_string:
-.next_char:
-    lodsb
-    cmp al, 0
-    je .done
-    mov ah, 0x0E
+do_reboot:
+    ; write buffer to sector 2 of first HDD (0x80)
+    mov ah,0x03        ; write sectors
+    mov al,1           ; number of sectors
+    mov ch,0           ; cylinder
+    mov cl,2           ; sector number
+    mov dh,0           ; head
+    mov dl,0x80        ; drive = first HDD
+    mov bx,slot        ; buffer to write
+    int 13h
+    jmp 0xFFFF:0
+
+do_shutdown:
+ ; write buffer to sector 2 of first HDD (0x80)
+    mov ah,0x03        ; write sectors
+    mov al,1           ; number of sectors
+    mov ch,0           ; cylinder
+    mov cl,2           ; sector number
+    mov dh,0           ; head
+    mov dl,0x80        ; drive = first HDD
+    mov bx,slot         ; buffer to write
+    int 13h
+    mov ax,0x03
     int 0x10
-    jmp .next_char
-.done:
+    cli
+.halt: hlt
+    jmp .halt
+
+; --- boot sound with BIOS delays ---
+boot_sound:
+    ; DU tone (~400 Hz, 200ms)
+    mov ax, 1193180/500
+    call speaker_on
+    mov cx, 0x0006
+    mov dx, 0x1A80     ; BIOS tick approx conversion
+    mov ah, 0x86
+    int 0x15
+    call speaker_off
+
+    ; Pause 1s
+    mov cx, 0x0006
+    mov dx, 0x1A80       ; ~1,000,000 microseconds
+    mov ah, 0x86
+    int 0x15
+
+    ; BING tone (~1000 Hz, 800ms)
+    mov ax, 1193180/1000
+    call speaker_on
+    mov cx, 0x0006
+    mov dx, 0x1A80        ; ~800,000 microseconds
+    mov ah, 0x86
+    int 0x15
+    call speaker_off
     ret
 
-; --- Input routine with backspace ---
+speaker_on:
+    mov bx, ax
+    mov al, 0xB6
+    out 0x43, al
+    mov al, bl
+    out 0x42, al
+    mov al, bh
+    out 0x42, al
+    in al, 0x61
+    or al, 3
+    out 0x61, al
+    ret
+
+speaker_off:
+    in al, 0x61
+    and al, 0xFC
+    out 0x61, al
+    ret
+
+; --- helpers ---
+print:
+    mov bx,si
+.p: mov al,[bx]
+    cmp al,0
+    je .d
+    mov ah,0x0E
+    int 10h
+    inc bx
+    jmp .p
+.d: ret
+
 read_line:
-    xor cx, cx          ; count of chars typed
-.next_key:
-    xor ah, ah
-    int 0x16            ; wait for key
-    cmp al, 0x0D        ; Enter?
+    xor cx,cx
+.rl: xor ah,ah
+    int 16h
+    cmp al,0x0D
     je .done
-    cmp al, 0x08        ; Backspace?
-    je .backspace
-
-    ; Normal character
-    stosb               ; store in buffer
-    inc cx              ; count++
-    mov ah, 0x0E        ; print char
-    int 0x10
-    jmp .next_key
-
-.backspace:
-    cmp cx, 0           ; nothing to delete?
-    je .next_key
-    dec di              ; move buffer pointer back
-    dec cx              ; reduce count
-    mov ah, 0x0E
-    mov al, 0x08        ; backspace cursor
-    int 0x10
-    mov al, ' '         ; overwrite with space
-    int 0x10
-    mov al, 0x08        ; move cursor back again
-    int 0x10
-    jmp .next_key
-
+    cmp al,0x08
+    je .back
+    stosb
+    inc cx
+    mov ah,0x0E
+    int 10h
+    jmp .rl
+.back:
+    cmp cx,0
+    je .rl
+    dec di
+    dec cx
+    mov ah,0x0E
+    mov al,0x08
+    int 10h
+    mov al,' '
+    int 10h
+    mov al,0x08
+    int 10h
+    jmp .rl
 .done:
-    mov al, 0
-    stosb               ; null terminator
+    mov al,0
+    stosb
     ret
 
-; --- Compare strings ---
-str_compare:
-.next_cmp:
-    mov al, [di]
-    cmp al, 0
-    je .match
-    cmp al, [si]
-    jne .nomatch
+cmp:
+    mov al,[di]
+.c: cmp al,0
+    je .m
+    cmp al,[si]
+    jne .n
     inc si
     inc di
-    jmp .next_cmp
-.match:
-    mov al, 1
+    mov al,[di]
+    jmp .c
+.m: mov al,1
     ret
-.nomatch:
-    mov al, 0
-    ret
-
-; --- Newline ---
-newline:
-    mov ah, 0x0E
-    mov al, 0x0D
-    int 0x10
-    mov al, 0x0A
-    int 0x10
+.n: mov al,0
     ret
 
-; -------------------------------
+nl:
+    mov ah,0x0E
+    mov al,0x0D
+    int 10h
+    mov al,0x0A
+    int 10h
+    ret
+
+; --- data ---
 prompt db '> ',0
-input_cmd db 'input',0
-echo_cmd db 'echo',0
-read_cmd db 'read',0
-unknown_msg db 'Unknown command',0
-input_msg db 'Enter text: ',0
-saved_msg db 'Saved!',0
-copy_msg db '2026 paz web any copying or distrbuting is strictly prohibited',0
+cmd_in db 'input',0
+cmd_echo db 'echo',0
+cmd_read db 'read',0
+cmd_reboot db 'reboot',0
+cmd_shut db 'shutdown',0
 
-buffer times 64 db 0
-slot   times 64 db 0   ; single slot for input
+msg_start db 'puff-OS',0
+msg_unknown db 'Unknown',0
 
-; --- Boot signature ---
+buf  times 12 db 0
+slot times 32 db 0
+
 times 510-($-$$) db 0
 dw 0xAA55
